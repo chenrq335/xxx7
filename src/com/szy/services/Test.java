d5c58eec6c2ff5e04a53f7332c0998cdc71554fd
chenrq335 <1015099078@qq.com>
2018-10-08 03:15:46 +0800

    first commit

---
+++ src/com/szy/services/Test.java
@@ -0,0 +1,13 @@
+package com.szy.services;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("chenrq demo");
+	}
+
+}
